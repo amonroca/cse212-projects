92d310633a78b98863976bd462fb2724bec255a2
amonroca <roc23009@byui.edu>
2025-07-04 20:38:35 -0300

    Implementação da contagem de duplicatas na classe DuplicateCounter e adição de lógica para tradução na classe Translator.

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,8 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        var notDuplicates = new HashSet<int>(data);
+
+        return data.Length - notDuplicates.Count;
     }
 }
