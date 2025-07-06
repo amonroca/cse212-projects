ad979f6556fc606282b7986a5c41495fe36bd17c
amonroca <roc23009@byui.edu>
2025-07-06 20:29:31 -0300

    Corrige a lógica de remoção no método Dequeue da PriorityQueue para garantir que o item com a maior prioridade seja removido corretamente.

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -24,7 +24,7 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++)
         {
             // FIX #3: Change the comparison to > so that the first item with the highest priority is removed.
             if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
@@ -33,6 +33,7 @@
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
         return value;
     }
 
